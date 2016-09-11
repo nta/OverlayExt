e9f308d7858fc10230d0c6be875fc923b0c9e0be
NTAuthority <bas@dotbas.net>
2016-09-11 09:46:35 +0200

    Initial premake5 template, doesn't do anything yet

---
+++ ayria_premake.lua
@@ -0,0 +1,59 @@
+function ayria_plugin(name)
+	workspace(name)
+		configurations { "Debug", "Release" }
+		platforms { "Win32", "x64" }
+
+		location "build/"
+
+		flags { "StaticRuntime", "No64BitChecks", "Symbols", "Unicode" }
+
+		flags { "NoIncrementalLink", "NoEditAndContinue" }
+
+		filter "platforms:Win32"
+			architecture "x32"
+
+		filter "platforms:Win64"
+			architecture "x64"
+
+		filter "configurations:Debug"
+			defines "_DEBUG"
+
+		filter "configurations:Release"
+			defines "NDEBUG"
+			optimize "Speed"
+
+		-- reset filter
+		filter()
+
+		targetdir "bin/%{cfg.platform}/%{cfg.buildcfg}/"
+		objdir "obj/%{cfg.platform}/%{cfg.buildcfg}/"
+
+	project(name)
+		language "C++"
+		kind "SharedLib"
+
+		targetname(name)
+
+		filter "platforms:Win32"
+			targetextension "Ayria32"
+
+		filter "platforms:Win64"
+			targetextension "Ayria64"
+
+		filter()
+
+		pchsource "src/StdInc.cpp"
+		pchheader "StdInc.h"
+
+		includedirs { "include/" }
+
+		files
+		{
+			"src/**.cpp",
+			"src/**.h",
+			"include/**.hpp",
+			"include/**.h",
+		}
+
+		postbuildcommands { "if exist \"$(SolutionDir)\\..\\postbuild.cmd\" call \"$(SolutionDir)\\..\\postbuild.cmd\" %{prj.name}" }
+end
---
+++ premake5.lua
@@ -0,0 +1,4 @@
+dofile('ayria_premake.lua')
+
+ayria_plugin 'OverlayExt'
+	links { 'd3d9' }
