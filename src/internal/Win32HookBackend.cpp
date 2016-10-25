399e0aaf324396d9d4e70ac29af25e49b0a4352d
NTAuthority <bas@dotbas.net>
2016-10-25 08:42:59 +0200

    add logging for API lookup

--- src/internal/Win32HookBackend.cpp
+++ src/internal/Win32HookBackend.cpp
@@ -22,9 +22,19 @@ namespace Hooking::Internal
 		// still no module? error out.
 		if (!hModule)
 		{
+            auto lastError = GetLastError();
+            FuncTrace("Could not load module %s (error code %d).\n", moduleName, lastError);
+
 			return nullptr;
 		}
 
-		return GetProcAddress(hModule, functionName);
+		void* pFunc = GetProcAddress(hModule, functionName);
+
+        if (!pFunc)
+        {
+            FuncTrace("Could not find function %s in module %s.\n", functionName, moduleName);
+        }
+
+        return pFunc;
 	}
 }
