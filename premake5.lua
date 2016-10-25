43b0288bce9ec1afaca3b34481ae089392ff6704
NTAuthority <bas@dotbas.net>
2016-10-25 07:49:40 +0200

    link MinHook from premake

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,30 @@
 dofile('ayria_premake.lua')
 
 ayria_plugin 'OverlayExt'
-	links { 'd3d9' }
+	links { 'd3d9', 'minhook' }
+
+    includedirs
+    {
+        'vendor/minhook/include',
+    }
+
+group 'vendor'
+project 'minhook'
+    language 'C'
+    kind 'StaticLib'
+
+    includedirs
+    {
+        'vendor/minhook/include'
+    }
+
+    files
+    {
+        'vendor/minhook/src/*.c'
+    }
+
+    filter 'platforms:Win32'
+        files 'vendor/minhook/src/HDE/hde32.c'
+
+    filter 'platforms:Win64'
+        files 'vendor/minhook/src/HDE/hde64.c'
