3187b125e9721c20bc6b0cae953c936626f606df
NTAuthority <bas@dotbas.net>
2016-10-25 13:16:55 +0200

    add a reference holder for COM hooks

--- include/ComHooking.h
+++ include/ComHooking.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <unordered_map>
+#include <concurrent_unordered_set.h>
+#include <thread>
+#include <mutex>
+
 #include <wrl.h>
 
 #include <boost/preprocessor/repeat.hpp>
-
-#include <concurrent_unordered_set.h>
 
 #pragma comment(lib, "rpcrt4.lib")
 
@@ -160,6 +163,211 @@ namespace Hooking::COM
 		// and return the new VMT
 		return newVmt;
 	}
+
+	class __declspec(uuid("F1326FC3-8E5E-450A-ACD1-4E796BA127CE")) IRefHolder : public IUnknown
+	{
+	public:
+		STDMETHOD(GetRefHolder)(void** refHolder) = 0;
+	};
+
+	template<typename T>
+	struct RefContainer
+	{
+		class RefHolder : public RuntimeClass<RuntimeClassFlags<ClassicCom>, IRefHolder>
+		{
+		private:
+			T m_refData;
+
+		public:
+			RefHolder()
+			{
+				hadRef = true;
+			}
+
+			RefHolder(const T& data)
+				: m_refData(data)
+			{
+				hadRef = true;
+			}
+
+			RefHolder(T&& data)
+				: m_refData(std::move(data))
+			{
+				hadRef = true;
+			}
+
+			T& GetData()
+			{
+				return m_refData;
+			}
+
+			const T& GetData() const
+			{
+				return m_refData;
+			}
+
+			STDMETHOD(GetRefHolder)(void** refHolder) override
+			{
+				*refHolder = &m_refData;
+				return S_OK;
+			}
+
+			bool hadRef;
+		};
+
+		struct RefMetaData
+		{
+			RefContainer container;
+			HRESULT(__stdcall* queryInterface)(IUnknown* self, REFIID riid, void** pObject);
+			ULONG(__stdcall* addRef)(IUnknown* self);
+			ULONG(__stdcall* release)(IUnknown* self);
+
+			RefMetaData()
+			{
+
+			}
+
+			RefMetaData(const RefContainer& container)
+				: container(container)
+			{
+
+			}
+		};
+
+		static RefMetaData* GetMetaData(void* object, RefContainer* baseContainer = nullptr)
+		{
+			static std::unordered_map<void*, RefMetaData> metaDataMap;
+			static std::mutex mutex;
+
+			std::unique_lock<std::mutex> lock(mutex);
+
+			auto it = metaDataMap.find(object);
+
+			if (it == metaDataMap.end())
+			{
+				if (baseContainer)
+				{
+					it = metaDataMap.insert({ object, RefMetaData(*baseContainer) }).first;
+				}
+				else
+				{
+					it = metaDataMap.insert({ object, RefMetaData() }).first;
+				}
+			}
+
+			return &it->second;
+		}
+
+		static HRESULT __stdcall QueryInterfaceStub(IUnknown* self, REFIID riid, void** pObject)
+		{
+			auto metaData = GetMetaData(self);
+
+			HRESULT hr = metaData->queryInterface(self, riid, pObject);
+
+			if (SUCCEEDED(hr))
+			{
+				if (!metaData->container.IsAttached((IUnknown*)*pObject))
+				{
+					metaData->container.Attach((IUnknown*)*pObject);
+				}
+				
+				// AddRef on the meta object - this needs two as some callers are.. weird
+				auto metaData = GetMetaData(*pObject);
+				auto rc = metaData->container.m_holder->AddRef();
+				rc = metaData->container.m_holder->AddRef();
+			}
+
+			return hr;
+		}
+
+		static ULONG __stdcall AddRefStub(IUnknown* self)
+		{
+			auto metaData = GetMetaData(self);
+			auto rc = metaData->container.m_holder->AddRef();
+
+			return metaData->addRef(self);
+		}
+
+		static ULONG __stdcall ReleaseStub(IUnknown* self)
+		{
+			auto metaData = GetMetaData(self);
+			auto rc = metaData->container.m_holder->Release();
+
+			return metaData->release(self);
+		}
+
+		RefContainer()
+		{
+			auto holder = Make<RefHolder>();
+			holder.CopyTo(&m_holder);
+		}
+
+		RefContainer(const T& data)
+		{
+			auto holder = Make<RefHolder>(data);
+			holder.CopyTo(&m_holder);
+		}
+
+		RefContainer(T&& data)
+		{
+			auto holder = Make<RefHolder>(std::move(data));
+			holder.CopyTo(&m_holder);
+		}
+
+		RefContainer(const RefContainer& container)
+		{
+			m_holder = container.m_holder;
+		}
+
+		RefContainer(IUnknown* object)
+			: RefContainer(GetMetaData(object)->container)
+		{
+
+		}
+
+		template<typename TInterface = IUnknown>
+		void Attach(IUnknown* object)
+		{
+			RefMetaData* metaData = GetMetaData(object, this);
+
+			void** vmt = EnsureCustomVmt<TInterface>(object);
+
+			if (vmt[0] != QueryInterfaceStub)
+			{
+				metaData->queryInterface = (decltype(metaData->queryInterface))vmt[0];
+				metaData->addRef = (decltype(metaData->addRef))vmt[1];
+				metaData->release = (decltype(metaData->release))vmt[2];
+			}
+
+			vmt[0] = QueryInterfaceStub;
+			vmt[1] = AddRefStub;
+			vmt[2] = ReleaseStub;
+
+			if (m_holder->hadRef)
+			{
+				m_holder->AddRef();
+				m_holder->hadRef = false;
+			}
+		}
+
+		bool IsAttached(IUnknown* object)
+		{
+			return (**(void***)object == QueryInterfaceStub);
+		}
+
+		T& Get()
+		{
+			return m_holder->GetData();
+		}
+
+		const T& Get() const
+		{
+			return m_holder->GetData();
+		}
+
+	private:
+		RefHolder* m_holder;
+	};
 
 	template<typename TFunc, typename TRawPtr = void*>
 	bool ReplaceVmtFunction(IUnknown* object, TFunc func, TRawPtr replacementFunction, TRawPtr* origFunction = nullptr)
